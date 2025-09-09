fa5dceefae66dc74f0bc4d2f67bbba697842d087
Arsham <soudbakhsha@gmail.com>
2025-09-09 20:45:15 +0330

    feat: integrate Categories and Menu components in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React from 'react';
-
+import React, { useState,useEffect } from 'react';
+import Menu from './Components/Menu'
+import Categories from './Components/Categories'
+import menu from "./data"
 
 function App() {
+  const [category, setCategory] = useState([]);
+  const [activeBtn, setActiveBtn] = useState("All");
+
+  const categories = menu.map((item) => {
+    return item.category;
+  });
+
+  let setCategoryItem = new Set(categories);
+
+  useEffect(() => {
+    setCategory(["All", ...setCategoryItem]);
+    
+  }, []);
+  
+  
 
   return (
-      <div>App</div>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories category={category} setCategory={setCategory} activeBtn={activeBtn} setActiveBtn={setActiveBtn}/>
+      </section>
+      <Menu activeBtn={activeBtn}/>
+    </main>
     
   );
 }
 
 export default App;
+
+
 
 
